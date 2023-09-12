f20b2dc92d3fc352c98252a0ef0eef5ad81d0ee9
Ahmet-SAAT <avahmetsaat@gmail.com>
2023-09-12 21:53:46 +0300

    first commit

---
+++ myapp/src/App.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import Kisiler from './components/c8/Kisiler'
+/* import Ulkeler from './components/c7/Ulkeler' */
+/* import Products from './components/c6/Products' */
+/* import Tablo from './components/c5/Tablo' */
+/* import SelectedCourses from './components/c4/SelectedCourses' */
+/* import Filtered from './components/c3/Filtered' */
+/* import IsLogin from './components/c2/IsLogin' */
+/* import Dersler from './components/c1/Dersler' */
+
+const App = () => {
+  return (
+    <div>
+    {/* 
+    
+<Dersler/> 
+<IsLogin/>    
+<Filtered/>    
+<SelectedCourses/>    
+<Tablo/>    
+<Products/>
+<Ulkeler/>
+
+
+
+
+
+
+    
+    */}
+
+<Kisiler/>
+
+
+
+
+
+
+
+
+    </div>
+  )
+}
+
+export default App
